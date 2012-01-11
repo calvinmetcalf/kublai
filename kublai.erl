9e6ef17370876bcf477f037203bfec4e7627abf9
Calvin Metcalf <calvin.metcalf@state.ma.us>
2012-01-11 15:56:25 -0500

    cleaner code

--- kublai.erl
+++ kublai.erl
@@ -1,14 +1,8 @@
 -module(kublai).
 -compile(export_all).
 writeTile(Map,X,Y,Z)->
-Path = filename:absname(""),
-Inname = lists:concat([Map, ".mbtiles"]),
-Pathparts = [Path,"tiles",Inname],
-Tileset = filename:join(Pathparts),
-{ok, Db} = sqlite3:start_link(mpo,[{file, Tileset}]),
-Qparts = ["SELECT tile_data FROM tiles WHERE zoom_level = ", Z, "  AND tile_column = ", X, " AND tile_row = ", Y],
-Sql = lists:concat(Qparts),
-[{columns,["tile_data"]},{rows,[{{blob,Tile}}]}] = sqlite3:sql_exec(Db, Sql),
-Outputlist = [Y, ".png"],
-Outname = lists:concat(Outputlist),
-file:write_file(Outname,Tile).
+{ok, Db} = sqlite3:start_link(mpo,[{file, filename:join([filename:absname(""),"tiles",lists:concat([Map, ".mbtiles"])])}]),
+
+[{columns,["tile_data"]},{rows,[{{blob,Tile}}]}] = sqlite3:sql_exec(Db, lists:concat(["SELECT tile_data FROM tiles WHERE zoom_level = ", Z, "  AND tile_column = ", X, " AND tile_row = ", Y])),
+
+file:write_file(lists:concat([Y, ".png"]),Tile).
