c2dbcaec1e141fc12d562ee98c0f6863cc3682ea
Calvin Metcalf <calvin.metcalf@state.ma.us>
2012-01-11 15:48:56 -0500

    should work on windows too

--- kublai.erl
+++ kublai.erl
@@ -2,8 +2,9 @@
 -compile(export_all).
 writeTile(Map,X,Y,Z)->
 Path = filename:absname(""),
-Pathparts = [Path,"/tiles/",Map,".mbtiles"],
-Tileset = lists:concat(Pathparts),
+Inname = lists:concat([Map, ".mbtiles"]),
+Pathparts = [Path,"tiles",Inname],
+Tileset = filename:join(Pathparts),
 {ok, Db} = sqlite3:start_link(mpo,[{file, Tileset}]),
 Qparts = ["SELECT tile_data FROM tiles WHERE zoom_level = ", Z, "  AND tile_column = ", X, " AND tile_row = ", Y],
 Sql = lists:concat(Qparts),
