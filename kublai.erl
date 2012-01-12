8f14db360d599035de353c21efbf0c4b3743db7a
Calvin Metcalf <calvin.metcalf@state.ma.us>
2012-01-12 14:08:23 -0500

    now can get info

--- kublai.erl
+++ kublai.erl
@@ -1,15 +1,14 @@
 -module(kublai).
 -compile(export_all).
-getTile(Map,Z,X,Y)->
-{ok, Db} = sqlite3:start_link(Map,[{file, filename:join([filename:absname(""),"tiles",lists:concat([Map, ".mbtiles"])])}]),
-Tilename = lists:concat([Y, ".png"]),
+getTile(M,Z,X,Y)->
+{ok, Db} = sqlite3:start_link(M,[{file, filename:join([filename:absname(""),"tiles",lists:concat([M, ".mbtiles"])])}]),
 [{columns,["tile_data"]},{rows,[{{blob,Tile}}]}] = sqlite3:sql_exec(Db, lists:concat(["SELECT tile_data FROM tiles WHERE zoom_level = ", Z, " AND tile_column = ", X, " AND tile_row = ", Y])),
 sqlite3:close(Db),
-[Tilename, Tile].
+Tile.
 
-getTileName(Map,Z,X,Y) ->
-hd(getTile(Map,Z,X,Y)).
-
-getTileBin(Map,Z,X,Y) ->
-tl(getTile(Map,Z,X,Y)).
+getInfo(M, K) ->
+{ok, Db} = sqlite3:start_link(M,[{file, filename:join([filename:absname(""),"tiles",lists:concat([M, ".mbtiles"])])}]),
+[{columns,["value"]},{rows,[{V}]}] = sqlite3:sql_exec(Db, lists:concat(["SELECT value FROM metadata WHERE 'name' = ", K])),
+sqlite3:close(Db),
+V.
 
