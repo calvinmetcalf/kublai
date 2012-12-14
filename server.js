33a88098f765466c2bfe75dfa12de7e1171290a9
Calvin Metcalf <calvin.metcalf@state.ma.us>
2012-12-14 13:18:50 -0500

    multi?

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
 #!/bin/env node
 require('coffee-script');
-require('./server.coffee');
+var cluster = require('cluster');
+var numCPUs = require('os').cpus().length;
+
+if (cluster.isMaster) {
+  // Fork workers.
+  for (var i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', function(worker, code, signal) {
+    console.log('worker ' + worker.process.pid + ' died');
+  });
+} else {
+  // Workers can share any TCP connection
+  // In this case its a HTTP server
+  require('./server.coffee');
+}
